682984a84b33c7ad0328c9c16f7bd4e090f379c9
kevinoliveiraferreira-prog <kevin.oliveira.ferreira@escola.pr.gov.br>
2026-08-06 10:43:19 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//Botões
+
+const btnContraste = document.getElementById("bnt-constraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto")
+const btnDiminuir = document.getElementById("btn-diminuir-texto")
+
+//Mudar tamanho da fonte
+
+let tamanhoAtualFonte = 100;
+
+//alto constraste
+
+btnContraste.addEventListener("click", ()=>{
+    document.bodyI.classList.
+})
